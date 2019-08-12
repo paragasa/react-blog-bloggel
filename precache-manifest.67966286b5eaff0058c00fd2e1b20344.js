83729dd3fb8014da6cff792caf57281b002e5e36
Alan Paul Paragas <alanpaulparagas@Alans-MacBook-Pro.local>
2019-08-12 03:04:19 -0700

    Updates

---
+++ precache-manifest.67966286b5eaff0058c00fd2e1b20344.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa81b2d1f51ba4b010e609a51920338",
+    "url": "/index.html"
+  },
+  {
+    "revision": "720e56772ba37f4b31d3",
+    "url": "/static/css/2.2fe65d49.chunk.css"
+  },
+  {
+    "revision": "720e56772ba37f4b31d3",
+    "url": "/static/js/2.522ee9f5.chunk.js"
+  },
+  {
+    "revision": "2491963c0bca88ad65a4",
+    "url": "/static/js/main.3c76b4bb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
